915bbdc657ee8c0f4c27d86848737cde9a5c692d
Yoshitada Kurozumi <8535627+KurozumiGH@users.noreply.github.com>
2024-05-18 23:26:17 +0900

    Support OpenAI GPT-4o

--- types/openai.ts
+++ types/openai.ts
@@ -7,46 +7,26 @@ export interface OpenAIModel {
   tokenLimit: number;
 }
 
+// OpenAI Models: https://platform.openai.com/docs/models
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
-  GPT_4_TURBO = 'gpt-4-1106-preview',
+  OPENAI_GPT_35_TURBO = 'gpt-3.5-turbo',
+  OPENAI_GPT_4O = 'gpt-4o',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_4_TURBO;
+export const fallbackModelID = OpenAIModelID.OPENAI_GPT_4O;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  [OpenAIModelID.OPENAI_GPT_35_TURBO]: {
+    id: OpenAIModelID.OPENAI_GPT_35_TURBO,
+    name: 'OpenAI GPT-3.5 Turbo',
+    maxLength: 16385 * 4,
+    tokenLimit: 16385,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-  [OpenAIModelID.GPT_4_TURBO]: {
-    id: OpenAIModelID.GPT_4_TURBO,
-    name: 'GPT-4-Turbo',
-    maxLength: 96000 * 4,
-    tokenLimit: 32000 * 4,
+  [OpenAIModelID.OPENAI_GPT_4O]: {
+    id: OpenAIModelID.OPENAI_GPT_4O,
+    name: 'OpenAI GPT-4o',
+    maxLength: 128000 * 4,
+    tokenLimit: 128000,
   },
 };
